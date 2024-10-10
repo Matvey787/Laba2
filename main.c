15dafa5c2e3e40bd5296aa80bc3e0098af4367d5
Matvey787 <matveyklg@gmail.com>
2024-10-10 15:52:57 +0300

    Laba2

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <math.h>
+
+
+double model(double *radioactivity, double *time, int N, double point){
+    double summary = 0;
+    int i = 0;
+
+    while (i < N) summary += time[i]*expf(-1*time[i]/point)*(radioactivity[i] - expf(-1*time[i++]/point));
+
+    return summary;
+}
